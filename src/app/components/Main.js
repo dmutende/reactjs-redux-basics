29d39d8823280584c4eb5a69e121f9185cc889e1
Derek Prince <princederek73@gmail.com>
2017-08-16 00:54:12 +0300

    Maisha ngumu mse!

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,22 +1,26 @@
 import React from "react";
 
-export class Main extends React.Component {
-    render() {
-        return (
-            <div>
-                <div className="row">
-                    <div className="col-xs-12">
-                        <h1>The Main Page</h1>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-12">
-                        <button
-                            className="btn btn-primary"
-                            onClick={() => this.props.changeUsername('Anna')}>Change the Username</button>
-                    </div>
+export const Main = (props) => {
+    return (
+        <div>
+            <div className="row">
+                <div className="col-xs-12">
+                    <h1>The Main Page</h1>
                 </div>
             </div>
-        );
-    }
-}
+            <div className="row">
+                <div className="col-md-12">
+                    <form className="form-inline">
+                        <div className="form-group">
+                            <input className="form-control" type="text" />
+                            <button
+                                className="btn btn-primary"
+                                onClick={() => props.changeUsername('Alexa')}>Change the Username
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
